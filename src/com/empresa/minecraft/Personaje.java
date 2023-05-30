472b51247e3c9ac9853eb54e7590a1bbd0d372d8
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-05-30 22:14:54 +0100

    Feat(Personaje.java):  Implementada la interfaz Combate,  Al atacar el metodo retorna la fuerza, y al recibirAtaque, aparece el daño y la salud restante.

--- src/com/empresa/minecraft/Personaje.java
+++ src/com/empresa/minecraft/Personaje.java
@@ -1,6 +1,6 @@
 package com.empresa.minecraft;
 
-public class Personaje {
+public class Personaje implements Combate {
     private int salud ;
     private int defensa;
     private int fuerza;
@@ -10,6 +10,19 @@ public class Personaje {
         this.salud = 12;
         this.fuerza = fuerza;
         this.defensa = defensa;
+    }
 
+    @Override
+    public int atacar() {
+        return fuerza;
+    }
+
+    @Override
+    public recibirAtaque(int ataque) {
+        int dañoRecibido = ataque - defensa;
+        salud -= dañoRecibido;
+
+        System.out.println("Le acaban de mandar una ostia al personaje de " + dañoRecibido);
+        System.out.println("Le queda " + salud + " para que se te vaya al cementerio de cabeza");
     }
 }

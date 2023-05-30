9da07b0ad5d709182a1867dd1ef80184ade2f82f
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-05-30 21:09:38 +0100

    feat(Personaje.java): Clase creada, declarados sus atributos junto a su constructor.

---
+++ src/com/empresa/minecraft/Personaje.java
@@ -0,0 +1,15 @@
+package com.empresa.minecraft;
+
+public class Personaje {
+    private int salud ;
+    private int defensa;
+    private int fuerza;
+
+
+    public Personaje(int defensa, int fuerza) {
+        this.salud = 12;
+        this.fuerza = fuerza;
+        this.defensa = defensa;
+
+    }
+}

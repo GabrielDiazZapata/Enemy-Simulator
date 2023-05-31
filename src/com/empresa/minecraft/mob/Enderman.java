7028a3161df3e6f5bdecb77774bab49950f191c7
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-05-31 17:41:39 +0100

    Feat: Creadas 3 clases una de cada Mob, con diferentes atrtibutos.

---
+++ src/com/empresa/minecraft/mob/Enderman.java
@@ -0,0 +1,12 @@
+package com.empresa.minecraft.mob;
+
+public class Enderman extends MobHostil {
+    public Enderman(){
+        super(14, 6);
+    }
+
+    public void moverse() {
+        System.out.println("El Enderman se te tepeo detras ahora que mi loco");
+    }
+    
+}
---
+++ src/com/empresa/minecraft/mob/Oveja.java
@@ -0,0 +1,12 @@
+package com.empresa.minecraft.mob;
+
+public class Oveja extends MobPacifico {
+    public Oveja() {
+        super(5);
+    }
+
+    public void moverse(){
+        System.out.println("La ovejita se va moviendo por el campito");
+    }
+    
+}
